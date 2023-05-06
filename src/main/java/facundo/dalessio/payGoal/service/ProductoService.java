3f6b4bdf7f2e5228d6204fb2d43e3ce2bcf574f2
FacuDalessio <fdalessio22@gmail.com>
2023-05-06 19:45:49 -0300

    Se agrego documentacion

--- src/main/java/facundo/dalessio/payGoal/service/ProductoService.java
+++ src/main/java/facundo/dalessio/payGoal/service/ProductoService.java
@@ -7,11 +7,55 @@ import java.util.List;
 
 public interface ProductoService {
 
+    /**
+     * Metodo que recive una request con los datos del producto, lo convierte a una entidad, lo guardo y por ultimo lo convierte en una response para retornar
+     * @param request request con los datos del producto a guardar
+     * @return retorna una response del producto guardado
+     */
     public ProductoResponse save(ProductoRequest request);
+
+    /**
+     * se busca un producto y se actualiza
+     * @param id id por el cual se busca al producto a actualizar
+     * @param request variable donde estan los datos para actualizar el producto
+     * @return retorna una response con los datos del producto
+     * @throws Exception tira una excepcion si no encuentra un producto con el id pasado
+     */
     public ProductoResponse update(Long id, ProductoRequest request) throws Exception;
+
+    /**
+     * busca y retorna una lista con todos los productos
+     * @return una lista con todos los productos
+     */
     public List<ProductoResponse> getProductos();
+
+    /**
+     * busca y retorna un producto con el id pasado
+     * @param id id por el cual se va a buscar el producto
+     * @return retorna un response con los datos del producto encontrado
+     * @throws Exception tira una excepcion si no encuentra un producto con el id pasado
+     */
     public ProductoResponse getProductoById(Long id) throws Exception;
+
+    /**
+     * busca y retorna un producto con el nombre pasado
+     * @param nombre nombre por el cual se va a buscar el producto
+     * @return retorna un response con los datos del producto encontrado
+     * @throws Exception tira una excepcion si no encuentra un producto con el nombre pasado
+     */
     public ProductoResponse getProductoByNombre(String nombre) throws Exception;
+
+    /**
+     * busca y borra un producto por el id pasado
+     * @param id id por el cual se va a buscar y borrar el producto
+     * @return retorna una cadena dicienco que se pudo borrar correctamente
+     * @throws Exception tira una excepcion si no encuentra un producto con el id pasado
+     */
     public String deleteProductoById(Long id) throws Exception;
+
+    /**
+     * busca y retorna una lista con todos los productos ordenados por precio
+     * @return una lista con todos los productos ordenados por precio
+     */
     public List<ProductoResponse> getProductosOrderByPrecio();
 }
--- src/main/java/facundo/dalessio/payGoal/utils/ComparadorPorPrecio.java
+++ src/main/java/facundo/dalessio/payGoal/utils/ComparadorPorPrecio.java
@@ -6,15 +6,21 @@ import java.util.Comparator;
 
 public class ComparadorPorPrecio implements Comparator<ProductoEntity> {
 
+    /**
+     * compara dos productos por precio
+     * @param p1 the first object to be compared.
+     * @param p2 the second object to be compared.
+     * @return si devuelve 0 son iguales, si devuelve -1 el segundo es mayor y si devuelve 1 el primero es mayor
+     */
     @Override
     public int compare(ProductoEntity p1, ProductoEntity p2) {
         int retAux = 0;
 
-        if (p1.getPrecio() - p2.getPrecio() > 0){
+        if (p1.getPrecio() > p2.getPrecio()){
             return 1;
         }
 
-        if (p1.getPrecio() - p2.getPrecio() < 0){
+        if (p1.getPrecio() < p2.getPrecio()){
             return -1;
         }
 
